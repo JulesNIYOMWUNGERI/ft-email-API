95b466d21764eaece7e17adecac5b4c533bab9cc
nshutiparfait99@gmail.com <nshutiparfait99@gmail.com>
2023-03-08 15:37:11 +0300

    feat: Project Structure
    - set up an express server
    - configure it to use Sequelize ORM and Postgres Database
    
    [Delivers hEJG6STn]

---
+++ models/user.model.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  });
+
+  return User;
+};
